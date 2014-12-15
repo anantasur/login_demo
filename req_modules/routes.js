4da6489c8273a0d76400d11ac4b6c526047e96d2
Ananthu R V <ananthurv666@hotmail.com>
2014-12-15 11:01:27 +0530

    added new db, made record manage ment

--- req_modules/routes.js
+++ req_modules/routes.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.view_dashboard  = function(req,res){
-	console.log('here');
+	console.log('in view_dashboard.js')
+	res.json(req.body);
 	res.render("dashboard");
 };
 
